e45d153c30a2f6b71a1e19adff57e23762e74846
MagadiM <milliemagadi4@gmail.com>
2022-07-05 10:39:53 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**main displays -putchar followed by a new line
+ * return 0 always
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+return (0);
+}
