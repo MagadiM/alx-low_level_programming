f5bdc47c20fca516103aadeae7b894e2982b44ee
MagadiM <milliemagadi4@gmail.com>
2022-07-06 02:44:18 -0700

    c hello world completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
  *main -> main displays the statement provided followed by a new line
- *int main (void) shows the function takes no arguements
+ *Description: 'the print will print out with proper grammar, but the outcome is a piece of art
  *return 0 always (success)
  */
 int main(void)
 {
-/*printf- prints with proper grammar, but the outcome is a piece of art*/
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
