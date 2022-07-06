cfbe83476f8922bcf62e3e493217d43f3d3c5ea8
MagadiM <milliemagadi4@gmail.com>
2022-07-06 02:20:10 -0700

    c hello world completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  *main -> main displays the statement provided followed by a new line
+ *int main (void) shows the function takes no arguements
  *return 0 always (success)
  */
 int main(void)
