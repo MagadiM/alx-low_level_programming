0f4640bc8d710a698b94cfcac5a3af717ffca8b7
MagadiM <milliemagadi4@gmail.com>
2022-07-06 01:56:28 -0700

    c hello world completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main -> displays the statement provided followed by a new line
+ *main -> main displays the statement provided followed by a new line
  *return 0 always (success)
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
