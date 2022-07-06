dd8dde18d3680ab393852ff8a92d5f57f8f067a2
MagadiM <milliemagadi4@gmail.com>
2022-07-06 06:16:24 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+}
