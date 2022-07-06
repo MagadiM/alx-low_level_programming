3c5bc94494e7f8a8ee7e1f28b130f2590c74c098
MagadiM <milliemagadi4@gmail.com>
2022-07-06 02:40:18 -0700

    c hello world completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
+/*printf- prints with proper grammar, but the outcome is a piece of art*/
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
