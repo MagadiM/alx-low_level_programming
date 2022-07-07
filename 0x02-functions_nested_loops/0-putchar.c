28708bf1da444696861dd48c8755ec563916efb3
MagadiM <milliemagadi4@gmail.com>
2022-07-06 18:52:19 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	-putchar(95);
-	-putchar(112);
-	-putchar(117);
-	-putchar(116);
-	-putchar(99);
-	-putchar(104);
-	-putchar(97);
-	-putchar(114);
+	-putchar('95');
+	-putchar('112');
+	-putchar('117');
+	-putchar('116');
+	-putchar('99');
+	-putchar('104');
+	-putchar('97');
+	-putchar('114');
 	-putchar('\n');
-	return(0);
+	return (0);
 }
