36e447921a5cc5582281e02057417e61ca82a46d
MagadiM <milliemagadi4@gmail.com>
2022-07-07 06:26:52 -0700

    print except qe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -> prints all the letters except q and e
+ * putchar; print out the letters
+ * return: 0 always
+ */
+int main(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
