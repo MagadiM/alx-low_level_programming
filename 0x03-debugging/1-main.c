27edee591b840dac9d4b8f3b5090b87b759887ed
MagadiM <milliemagadi4@gmail.com>
2022-07-07 20:49:40 -0700

    debugging task 2

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include  <stdio.h>
+
+/**
+ *main - causes an infinite loop
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+/**
+*while (i < 10) setting prameters for main
+*{
+*	putchar(i);
+*}
+*/
+
+	printf("Infinite loop avoided! \\o/\n"); /*indicates character termination*/
+
+	return (0);
+}
