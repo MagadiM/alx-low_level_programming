1bd4bdd0da133fd693f0b8d38c1335d0251012b0
MagadiM <milliemagadi4@gmail.com>
2022-07-08 18:57:47 -0700

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
 /**
- * mul - displays the multiplication of two integers
- *
- * @a - input integer
- *
- * @b - displays input integer
- *
- * Return : 0 always
+ * mul - a function that multiplies two integers.
+ * @a: An input integer
+ * @b: An input integer
+ * Return: Always 0
  */
 int mul(int a, int b)
 {
-	for (a = 1; a <= 9; a++)
-	{
-		for (b = 1; b <= 9; b++)
-		{
-			printf("%d", (a * b));
-		}
-		printf("\n");
-	}
+	return (a * b);
 }
