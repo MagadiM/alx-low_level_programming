0898d1db0f9d3d48f85e3eacb17fadeebb69d058
MagadiM <milliemagadi4@gmail.com>
2022-07-09 19:28:13 -0700

    size fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 /**
- *main print the size of the various types
- *Return; 0
+ * main - program entry point
+ * void - there are no arguements
+ *
+ * Return 0 to exit
  */
 int main(void)
+
 {
+
 	/*size of operator is used to evaluate the size of the variable*/
-	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-	printf("Size of a int: %ld byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long intType));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long intType));
-	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %d byte(s)\n", sizeof(charType));
+	printf("Size of a int: %d byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long intType));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long intType));
+	printf("Size of a float: %d byte(s)\n", sizeof(floatType));
 	return (0);
 }
