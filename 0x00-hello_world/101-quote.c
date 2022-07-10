4cc2bc6e611d604570eb89f5dd0c6fb0c6f14a0a
MagadiM <milliemagadi4@gmail.com>
2022-07-09 20:10:30 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,14 @@
 #include <unistd.h>
 
 /**
- * main-prints exactly "and that piece of art is useful" Dora Korpar, 2015-10-19"
- * followed by standard error
- * return : always 0 (success)
+ * main - prints exactly "and that piece of art is useful"
+ * Description; program description
+ * Return : always 0 (success)
  */
+
 int main(void)
+
 {
-	write(2, "and that piece of art is useful\" Dora Korpar, 2015-10-19\n59);
+	write(2, "and that piece of art is useful\" Dora Korpar, 2015-10-19\n" 59);
 	return (1);
 }
