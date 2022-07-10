11bc1d79c73d909e0c7338d2c3fabdba6ebd1ddb
MagadiM <milliemagadi4@gmail.com>
2022-07-09 20:14:09 -0700

    fixer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * Description; program description
- * Return : always 0 (success)
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" Dora Korpar, 2015-10-19\n" 59);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
+
 }
