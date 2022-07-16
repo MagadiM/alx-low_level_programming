1394a906eb440fe196e435f6e99501455bde0c28
MagadiM <milliemagadi4@gmail.com>
2022-07-16 01:06:28 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - assigns values to integer n
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int n;
+
+	for (n >= 1; n <= 9; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
